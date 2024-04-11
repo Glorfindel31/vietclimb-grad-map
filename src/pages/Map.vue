d4f82528682c26d2fdfe5694d638976b8cc621a9
glorfindel31 <cedric31flo@gmail.com>
2024-04-11 22:14:25 +0700

    hover with names on the mouse ready for refacto

--- src/pages/Map.vue
+++ src/pages/Map.vue
@@ -6,16 +6,12 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
-import { TTFLoader } from 'three/examples/jsm/loaders/TTFLoader';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-
 
 let camera, renderer, mesh, controls;
 
-
 let hoverMesh = null;
 const mapContainer = ref(null);
+const mouseBox = ref(null);
 const scene = new THREE.Scene();
 
 const raycaster = new THREE.Raycaster();
@@ -69,11 +65,38 @@ function onMouseMove(event) {
         }
         mesh.material = hoverMaterial;
         hoverMesh = mesh;
+        const windowX = event.clientX;
+        const windowY = event.clientY;
+        const boxWidth = mouseBox.value.offsetWidth;
+        const boxHeight = mouseBox.value.offsetHeight;
+
+        let hoverX = windowX + 20; // Adjust offset as needed
+        let hoverY = windowY + 10; // Adjust offset as needed
+
+        // Prevent hover box from going off screen
+        if (hoverX + boxWidth > window.innerWidth) {
+            hoverX = windowX - boxWidth - 10; // Adjust offset as needed
+        }
+        if (hoverY + boxHeight > window.innerHeight) {
+            hoverY = windowY - boxHeight - 10; // Adjust offset as needed
+        }
+
+        mouseBox.value.style.left = `${hoverX}px`;
+        mouseBox.value.style.top = `${hoverY}px`;
+        mouseBox.value.style.visibility = 'visible';
+        mouseBox.value.textContent = mesh.name
+            .replace(/\d+/g, '') // Remove all digits
+            .replace(/_/g, ' ') // Replace underscores with spaces
+            .replace(/\b\w/g, function (l) { return l.toUpperCase() }) // Capitalize the first letter of each word
+            .replace(/\s\w/g, function (l) { return l.toLowerCase() }) // Ensure the rest of the letters are in lowercase
+            .replace(/([A-Z])/g, ' $1') // Add a space before each capital letter (excluding the first letter)
+            .trim(); // Remove any leading or trailing spaces
     } else {
         if (hoverMesh) {
             hoverMesh.material = wallMaterial;
             hoverMesh = null;
         }
+        mouseBox.value.style.visibility = 'hidden';
     }
 }
 
@@ -99,7 +122,7 @@ onMounted(() => {
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setClearColor(0x171717, 0.7);
-    renderer.setPixelRatio(window.devicePixelRatio * 2);
+    renderer.setPixelRatio(window.devicePixelRatio * 1);
     renderer.setSize(mapContainer.value.clientWidth, mapContainer.value.clientHeight);
     renderer.shadowMap.enabled = true;
 
@@ -124,7 +147,7 @@ onMounted(() => {
         controls = new OrbitControls(camera, renderer.domElement);
 
         // Add ambient light
-        const ambientLight = new THREE.AmbientLight(0xffffff, 1.4); // soft white light
+        const ambientLight = new THREE.AmbientLight(0xffffff, 1); // soft white light
 
 
 
@@ -132,35 +155,17 @@ onMounted(() => {
         const pointLight01 = new THREE.PointLight(0xffffff, 600, 800);
         pointLight01.position.set(-5, 20, -10); // Adjust position as needed
         pointLight01.castShadow = true;
-        pointLight01.shadow.mapSize.width = 1500; // default is 512
-        pointLight01.shadow.mapSize.height = 1500; // default is 512
+        pointLight01.shadow.mapSize.width = 500; // default is 512
+        pointLight01.shadow.mapSize.height = 500; // default is 512
         pointLight01.shadow.camera.near = 0.2; // default
         pointLight01.shadow.camera.far = 10; // default
 
         scene.add(ambientLight);
         scene.add(pointLight01);
 
-        const fontLoader = new FontLoader();
-
-        fontLoader.load(
-            'node_modules/three/examples/fonts/helvetiker_regular.typeface.json',
-            (font) => {
-                const textGeometry = new TextGeometry('Hello, Three.js!', {
-                    font: font,
-                    size: 2,
-                    depth: 0.2,
-                })
-                const textMaterial = new THREE.MeshNormalMaterial();
-                const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-                textMesh.position.set(0, 5, 0);
-                scene.add(textMesh);
-            });
-
-
         // Load the GLTF model
         const loader = new GLTFLoader();
         loader.load('/VCmapThreeJS.glb', (gltf) => {
-
 
             // Add the model to the scene
             scene.add(gltf.scene);
@@ -204,7 +209,6 @@ onMounted(() => {
         window.addEventListener('resize', onWindowResize);
         renderer.domElement.addEventListener('mousemove', onMouseMove, false);
         window.addEventListener('mouseleave', revertMaterials, false);
-
     }
     // Animation camera
     const animate = () => {
@@ -242,6 +246,7 @@ onUnmounted(() => {
     renderer.domElement.removeEventListener('mousemove', onMouseMove, false);
     window.removeEventListener('mouseleave', revertMaterials, false);
 
+
 });
 </script>
 
@@ -249,4 +254,6 @@ onUnmounted(() => {
     <div class="min-w-full min-h-full border rounded-lg p-4">
         <div ref="mapContainer" class="w-full h-[80vh] overflow-hidden flex justify-center items-center"></div>
     </div>
+    <div class="border border-foreground absolute z-50 top-0 left-0 px-4 py-2 rounded-sm bg-background" ref="mouseBox"
+        id="mouseBox" style="visibility: hidden"></div>
 </template>
